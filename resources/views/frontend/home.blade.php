be82ed184719a47d70ce5d37ca3fe3c1bad13bba
Hassan Sultan <h.hsultan277@gmail.com>
2022-12-21 20:17:06 +0500

    hassan

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -3073,7 +3073,7 @@
                                                             alt="Drone camera">
 
                                                         <img class="img-fluid img2"
-                                                            src="@if ($row->singleImage != null) {{ asset('storage/' . $row->singleImage->image) }} @else {{ asset('public/storage/' . $row->fea_img) }} @endif"
+                                                            src="@if ($row->singleImage != null) {{ asset('public/storage/' . $row->singleImage->image) }} @else {{ asset('public/storage/' . $row->fea_img) }} @endif"
                                                             alt="Drone camera">
 
                                                     </a>
